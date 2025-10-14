722eec5165dd508394568edab6d8cb2d5ff98bce
Sergey Serebryakov <1047261+megaserg@users.noreply.github.com>
2025-10-14 09:21:50 -0700

    fix failing test

--- src/reduce.ts
+++ src/reduce.ts
@@ -15,14 +15,24 @@ export function reduceAnsiCodesIncremental(codes: AnsiCode[], newCodes: AnsiCode
 			// Reset code, disable all codes
 			ret = [];
 		} else if (endCodesSet.has(code.code)) {
-			// This is an end code, disable all matching start codes
-			ret = ret.filter((retCode) => retCode.endCode !== code.code);
+			// This is an end code, remove the most recently opened matching code
+			for (let i = ret.length - 1; i >= 0; i--) {
+				if (ret[i].endCode === code.code) {
+					ret.splice(i, 1);
+					break;
+				}
+			}
 		} else {
-			// This is a start code. Disable all styles this "overrides", then enable it
-			ret = ret.filter((retCode) => retCode.endCode !== code.endCode);
-			ret.push(code);
+			// This is a start code. Remove codes it "overrides", then add it.
+			// If a new code has the same endCode, it "overrides" existing ones.
+			// Special case: Intensity codes (1m, 2m) can coexist (both end with 22m).
+			const isIntensityCode = code.code === "\u001B[1m" || code.code === "\u001B[2m";
+				if (!isIntensityCode) {
+					ret = ret.filter((retCode) => retCode.endCode !== code.endCode);
+				}
+				ret.push(code);
+			}
 		}
+		return ret;
 	}
-
-	return ret;
-}
+	
